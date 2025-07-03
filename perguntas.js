42fc6759e0ab67063d1289466cc9f0344796531b
GUELGUEL22 <constantino.miguel@escola.pr.gov.br>
2025-07-03 15:33:36 -0300

    3.5

--- perguntas.js
+++ perguntas.js
@@ -18,7 +18,7 @@ criaCartao(
 
 criaCartao(
     'Ciências',
-    'CO que é fotossíntese??',
+    'O que é fotossíntese??',
     'É o processo onde plantas são produzidas como alimento usando luz solar, água e dióxido de carbono.'
 )
 
