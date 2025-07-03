34797610e092517ddb0b944cc82dac37dc003cb5
GUELGUEL22 <constantino.miguel@escola.pr.gov.br>
2025-07-03 15:30:35 -0300

    3.4

--- perguntas.js
+++ perguntas.js
@@ -1,59 +1,59 @@
 criaCartao(
-    'MATEMÁTICA',
+    'Matemática',
     'O que é um número primo??',
     'É um número maior que 1 que só é divisível por 1 e por ele mesmo.'
 )
 
 criaCartao(
+    'História',
+    'Quem foi Napoleão Bonaparte?',
+    'Foi um líder militar e imperador francês do início do século 19.'
+)
+
+criaCartao(
     'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Qual ​​é o maior oceano do mundo?',
+    'O Oceano Pacífico é o maior oceano do planeta.'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Ciências',
+    'CO que é fotossíntese??',
+    'É o processo onde plantas são produzidas como alimento usando luz solar, água e dióxido de carbono.'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
-)
-
-criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Português',
+    'O que é um verbo no infinitivo?',
+    'É a forma básica do verbo, sem conjugação, como "cantar", "comer", "partir".'
 )
 
 criaCartao(
     'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Como se diz “Obrigado” em inglês?',
+    'Diz-se “Thank you”.'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Física',
+    'O que é a força gravitacional?',
+    'É a força que atrai os corpos com massa entre si, como a Terra atraindo objetos.'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Química',
+    'O que é um elemento químico?',
+    'É uma substância formada por átomos do mesmo tipo, como oxigênio ou ferro.'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Artes',
+    'Quem pintou a Mona Lisa?',
+    'Leonardo da Vinci, durante o Renascimento italiano.'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Educação Física',
+    'Qual exercício é usado para fortalecer pernas?',
+    'Agachamento é um exercício eficaz para fortalecer as pernas.'
 )
